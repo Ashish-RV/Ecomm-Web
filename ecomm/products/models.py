34645bf5a18367f30bd828e586cf3fd966f5adf8
Ashish RV <ashish14141@gmail.com>
2025-02-08 16:52:32 +0530

    sat mood

--- ecomm/products/models.py
+++ ecomm/products/models.py
@@ -21,10 +21,16 @@ class ColorVariant(BaseModel):
     color_name = models.CharField(max_length=100)
     price = models.IntegerField(default=0)
 
+    def __str__(self) -> str:
+        return self.color_name
+
+
 class SizaVariant(BaseModel):
     size_name = models.CharField(max_length=100) 
     price = models.IntegerField(default=0)  
 
+    def __str__(self) -> str:
+        return self.size_name
 
 
 
@@ -34,8 +40,8 @@ class Product(BaseModel):
     category = models.ForeignKey(Category, on_delete= models.CASCADE, related_name='products')
     price = models.IntegerField()
     product_description = models.TextField()
-    color_variant = models.ManyToManyField(ColorVariant)
-    size_variant = models.ManyToManyField(SizaVariant)
+    color_variant = models.ManyToManyField(ColorVariant, blank=True)
+    size_variant = models.ManyToManyField(SizaVariant, blank=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.product_name)
@@ -48,3 +54,10 @@ class Product(BaseModel):
 class ProductImage(BaseModel):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_images')
     image =  models.ImageField(upload_to='product')
+
+
+class Coupon(BaseModel):
+    coupon_code = models.CharField(max_length=10)
+    is_expired = models.BooleanField(default=False)
+    discount_price = models.IntegerField(default=100)
+    minimum_amount = models.IntegerField(default=500)
